e8636ef082d2f43737cee7ae791ca4ba29c547c1
张祖飘 <990881086@qq.com>
2024-04-23 15:45:33 +0800

    功能开发

--- src/DialogueMessageServiceProvider.php
+++ src/DialogueMessageServiceProvider.php
@@ -7,16 +7,16 @@ use Illuminate\Support\ServiceProvider;
 class DialogueMessageServiceProvider extends ServiceProvider
 {
     public function boot(){
-        $date = '2024_04_23';
+        $date = date('Y_m_d');
         $this->publishes([
             __DIR__.'/../config/dialogue.php' => base_path('config/dialogue.php'),
         ], 'config');
         $this->publishes([
-            __DIR__.'/../database/migrations/'.$date.'_000000_create_dialogues_table.php' => database_path('migrations/'.$date.'_000000_create_dialogues_table.php'),
-            __DIR__.'/../database/migrations/'.$date.'_000000_create_dialog_statuses_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_statuses_table.php'),
-            __DIR__.'/../database/migrations/'.$date.'_000000_create_dialog_members_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_members_table.php'),
-            __DIR__.'/../database/migrations/'.$date.'_000000_create_dialog_messages_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_messages_table.php'),
-            __DIR__.'/../database/migrations/'.$date.'_000000_create_dialog_message_statuses_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_message_statuses_table.php'),
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_dialogues_table.php' => database_path('migrations/'.$date.'_000000_create_dialogues_table.php'),
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_dialog_statuses_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_statuses_table.php'),
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_dialog_members_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_members_table.php'),
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_dialog_messages_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_messages_table.php'),
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_dialog_message_statuses_table.php' => database_path('migrations/'.$date.'_000000_create_dialog_message_statuses_table.php'),
         ], 'migrations');
 
     }
